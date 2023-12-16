1676c4c170702df69d544834927d1e30899ca794
Mukhammad Yarmukhamedov <104412046+Yarmukhamedov@users.noreply.github.com>
2023-12-16 15:35:00 +0500

    Add files via upload

---
+++ scripts/MyDietCoach.js
@@ -0,0 +1,297 @@
+var obj = JSON.parse($response.body);
+obj = 
+{
+  "transactions" : {
+
+  },
+  "me" : {
+    "non_consumables_ids" : [
+
+    ],
+    "active_subscriptions_ids" : [
+      "com.inspiredapp.mydietcoach.1w_t10_1w_bundle_bis"
+    ],
+    "privacy_notice" : {
+      "required_consents" : [
+
+      ],
+      "is_at_least_16" : "unknown",
+      "last_acknowledged_version" : "2.0.0"
+    },
+    "unique_id" : "C3133849-9E3B-44D0-9D14-F3139282AEF4",
+    "active_bundle_subscriptions" : [
+      {
+        "expiry" : "2099-03-23T06:40:58+00:00",
+        "product_id" : "com.inspiredapp.mydietcoach.1w_t10_1w_bundle_bis",
+        "features" : [
+          "unlock"
+        ]
+      }
+    ],
+    "is_spooner" : false,
+    "terms_of_service" : {
+      "last_accepted_version" : "1.0.0"
+    },
+    "available_consumable_credits" : {
+
+    }
+  },
+  "settings" : {
+    "__is_testflight__" : false,
+    "default_paywall_type" : "benefits",
+    "soft_monetization_type" : "delayed",
+    "__identity__" : {
+      "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImY1YWVmMWQ0ODg2YjQwZDY4MTIzZTk5OWQ4YWY5ZDljIn0.eyJwYXlsb2FkIjp7ImJhY2tlbmRfaWQiOiJDMzEzMzg0OS05RTNCLTQ0RDAtOUQxNC1GMzEzOTI4MkFFRjQiLCJ1c2VyX2lkIjoiRkJCMDlBNEEtQjM4Ny00ODdGLTg4NTYtOTk3MTgzRTA2N0RDIiwiaXNfZnJlZSI6ZmFsc2UsImlzX3Nwb29uZXIiOmZhbHNlLCJpc19zdWJzY3JpYmVkIjp0cnVlLCJjdXJyZW50X2luc3RhbGxhdGlvbl90aW1lIjoiMjAyMy0wMy0xNlQwNjozOToyMSswMDowMCIsImZpcnN0X2luc3RhbGxhdGlvbl90aW1lIjoiMjAyMy0wMy0xNlQwNjozOToyMSswMDowMCIsInByaXZhY3lfcG9saWN5Ijp7Imxhc3RfYWNrbm93bGVkZ2VkX3ZlcnNpb24iOiIyLjAuMCJ9LCJzZXR0aW5ncyI6e30sImNvbnN1bWFibGVfY3JlZGl0cyI6e319LCJpc3MiOiJvcmFjbGUyIiwiaWF0IjoxNjc4OTQ4OTc0LCJuYmYiOjE2Nzg5NDg5NzQsImV4cCI6MTY3OTAzNTM3NH0.p2gJA6LGQcKFAnwlm3Yy60n7G8iXvWyUXC2fnCobPcE",
+      "expiration" : "2023-03-17T06:42:54.770654+00:00"
+    },
+    "__terms_of_service_version__" : "1.0.0",
+    "main_subscription_index" : 0,
+    "paywall_title_id" : "standard",
+    "base_subscription_id" : "com.inspiredapp.mydietcoach.1w_t10_1w_bundle_bis",
+    "instant_paywall_subscription_id" : "com.bendingspoonsapps.berry.1w_t10_1w",
+    "review_soft_trigger_factor" : 3,
+    "__privacy_notice_version__" : "2.0.0",
+    "search_delay_seconds" : 0.5,
+    "review_hard_trigger_factor" : 1,
+    "comparison_paywall_monthly_subscription_index" : 17,
+    "bundle_apps_number" : 7,
+    "__privacy_policy_url__" : "https://support.bendingspoons.com/privacy.html?app=552341639&version=2.0.0",
+    "__terms_of_service_effective_date__" : "2021-05-25T12:00:00+00:00",
+    "comparison_paywall_weekly_subscription_index" : 16,
+    "monetization_type" : "soft",
+    "instant_paywall_type" : "benefits",
+    "__is_baseline__" : false,
+    "__encryption_public_key__" : "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6WMNkl4avAdHhYGc20hq\n8DcVtWanEtvuzM7g80Pe8Tv+eyrGyO1+XrgOUu7FRW2TFAl1q1r+3Uls0Y4Xyevi\n1F52DlSrKUJ84fpHIlhMhG8fGKTtAEOZ/CeSj4eUIjGqZe79/BAsEs1A5TZEKdlw\n24oeuMklhCeJ09Im/cU5FQJ18f6Ew8Dqmp7lLKsXoUA8hhBAPbcN7cb4cX6pvNWC\n+alZ9yX2sm4gs3cqjHPsLIEptJkaR469ZcSxqbBNtduS+DMRyhVD+M9ekjdv6aaE\nmRuFumWRtLtGvL9QfyOvTQPsp4eI/O4iZUH7A/W9L67WiYWDPhyknGd7fjIaEedS\n5wIDAQAB\n-----END PUBLIC KEY-----",
+    "skip_paywall" : false,
+    "barcode_visual" : "magnifier",
+    "off_track_threshold" : 5,
+    "instagram_url" : "https://www.instagram.com/berrythecoach",
+    "soft_trigger_factor" : 3,
+    "comparison_paywall_yearly_subscription_index" : 18,
+    "min_time_between_requests" : 600,
+    "idfa_popup_position" : "before_onboarding",
+    "facebook_url" : "https://www.facebook.com/BerryTheCoach/",
+    "idfa_popup_enabled" : false,
+    "__encryption_algorithm__" : "RSA2048PKCS1",
+    "__privacy_policy_version__" : "2.0.0",
+    "facebook_app_link" : "fb://profile/104393031149262",
+    "__terms_of_service_url__" : "https://support.bendingspoons.com/tos.html?app=552341639&version=1.0.0",
+    "__is_free__" : false,
+    "instagram_app_link" : "instagram://user?username=berrythecoach",
+    "review_native_prompt_chance" : 0,
+    "food_to_recipe_ratio" : 10,
+    "review_min_time_between_requests" : 600,
+    "default_paywall_type_2" : "benefits_bundle",
+    "__min_required_build_version__" : "0.0.0",
+    "instant_paywall_enabled" : false,
+    "review_show_native_prompt_chance" : 1,
+    "hard_trigger_factor" : 1,
+    "is_food_database_localized" : true,
+    "profile_socials_enabled" : false,
+    "__privacy_notice_url__" : "https://support.bendingspoons.com/privacy.html?app=552341639&version=2.0.0",
+    "iphone_min_ver" : 1,
+    "review_skip_survey" : false,
+    "review_prompt_displayed_cap" : 5,
+    "__encryption_key_id__" : "53bfd4440016c",
+    "instant_paywall_subscription_index" : 15,
+    "app_shutdown_date" : "10/04/2021",
+    "max_requests_per_version" : 2,
+    "__experiments__" : {
+
+    }
+  },
+  "force_updater" : {
+    "suggested_build_version" : "0.0.0",
+    "suggested_update_frequency" : 0,
+    "action" : "do_nothing"
+  },
+  "legal_notifications" : {
+    "to_notify_privacy_policy" : false,
+    "to_notify_terms_of_service" : false
+  },
+  "products" : {
+    "consumables" : [
+
+    ],
+    "subscriptions" : [
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.monthly_regular_itro_price",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.preimum_monthly_v2",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.preimum_yearly_v2",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_exclusive_v3",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_monthly_discounted",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_monthly_discounted_v3",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_monthly_discounted_v4",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_weekly_discounted_22_02_18",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_yearly_discounted",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_Yearly_Discounted_22Feb18",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premium_yearly_discounted_v3",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.premuim_yearly_discounted_v4",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.yearly_discounted_free_trial",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.yearly_regular_intro_price",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.yearly_special_offer",
+        "features" : [
+
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1w_t6_1w",
+        "features" : [
+          "unlock"
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1w_t6",
+        "features" : [
+          "unlock"
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1m_t14",
+        "features" : [
+          "unlock"
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1y_t60",
+        "features" : [
+          "unlock"
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1w_t8_bundle",
+        "features" : [
+          "unlock"
+        ]
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapp.mydietcoach.1w_t10_1w_bundle_bis",
+        "features" : [
+          "unlock"
+        ]
+      }
+    ],
+    "non_consumables" : [
+      {
+        "status" : "APPROVED",
+        "product_id" : "my_diet_coach_complete_discounted"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "my_diet_coach_diary_package_only"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "my_diet_coach_motivational_and_diary_package"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.inappupgrade"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.lifetime_complete"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.basic_package_v2"
+      },
+      {
+        "status" : "APPROVED",
+        "product_id" : "com.inspiredapps.mydietcoach.basic_us"
+      }
+    ]
+  }
+}
+$done({body: JSON.stringify(obj)});
