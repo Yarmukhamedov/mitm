70a892d882a67682eed4193e4cca4927f59c3945
Mukhammad Yarmukhamedov <104412046+Yarmukhamedov@users.noreply.github.com>
2023-12-17 11:51:28 +0500

    Create TidalHiFiPlus.js

---
+++ scripts/TidalHiFiPlus.js
@@ -0,0 +1,16 @@
+$done({
+    body: JSON.stringify({
+        "status": "ACTIVE",
+        "canGetTrial": false,
+        "startDate": "2019-09-28T09:09:09.000+0000",
+        "subscription": {
+            "type": "PREMIUM_PLUS",
+            "offlineGracePeriod": 999999
+        },
+        "premiumAccess": true,
+        "paymentType": "ADYEN_CREDIT_CARD",
+        "paymentOverdue": false,
+        "highestSoundQuality": "HI_RES",
+        "validUntil": "2999-09-28T09:09:09.000+0000"
+    })
+});
